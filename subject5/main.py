3e29d8b5318cf7757d58fb5908026bdd9befed3e
KIM TAEHYUNG <kth978340@gmail.com>
2026-04-08 13:02:18 +0900

    Delete subject5 directory

--- subject5/main.py
+++
@@ -1,8 +0,0 @@
-name = input("이름을 입력하세요: ")
-student_id = input("학번을 입력하세요: ")
-subject = input("학과를 입력하세요: ")
-print("\n")
-print("<출력>")
-print(f"이름: {name}")
-print(f"학번: {student_id}")
-print(f"학과: {subject}")
